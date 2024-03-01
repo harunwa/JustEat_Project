988a570e95007155a99ce134fca30a58aefc331b
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-01 21:02:49 +0000

    initial commit added dependencies

---
+++ justeat_project/src/main/java/com/example/justeat_project/JusteatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.justeat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusteatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusteatProjectApplication.class, args);
+	}
+
+}
