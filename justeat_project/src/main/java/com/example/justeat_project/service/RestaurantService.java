1f6dfeb5c930735f1f9d3f07aced89bbf23275d7
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-01 23:14:53 +0000

    added method for endpoint data retrieval

--- justeat_project/src/main/java/com/example/justeat_project/service/RestaurantService.java
+++ justeat_project/src/main/java/com/example/justeat_project/service/RestaurantService.java
@@ -1,7 +1,16 @@
 package com.example.justeat_project.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RestaurantService {
+
+    public void getRestaurantData(String postcode){
+        String apiUrl = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/" + postcode;
+        RestTemplate restTemplate = new RestTemplate();
+        String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+    }
+
+
 }
