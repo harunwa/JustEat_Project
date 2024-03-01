90b753429e682d3d358548513e4866b1f326118a
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-01 23:33:14 +0000

    added getcuisines method for cuisine data retrieval & formatting

--- justeat_project/src/main/java/com/example/justeat_project/service/RestaurantService.java
+++ justeat_project/src/main/java/com/example/justeat_project/service/RestaurantService.java
@@ -1,5 +1,6 @@
 package com.example.justeat_project.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,16 @@ public class RestaurantService {
         String apiUrl = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/" + postcode;
         RestTemplate restTemplate = new RestTemplate();
         String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+    }
+
+    public String getCuisines(JsonNode restaurant){
+        StringBuilder cuisines = new StringBuilder();
+        JsonNode cuisinesArray = restaurant.get("cuisines");
+        for (int i = 0; i<cuisinesArray.size(); i++){
+            JsonNode cuisine = cuisinesArray.get(i);
+            cuisines.append(cuisine.get("name").asText());
+        }
+        return cuisines.toString().replaceAll(", ,", "");
     }
 
 
