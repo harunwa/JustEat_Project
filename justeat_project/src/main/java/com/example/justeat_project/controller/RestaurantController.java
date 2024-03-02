29eba388d4f9bcb7effa24f48036911950f5d9da
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-02 14:09:39 +0000

    added autowired to controller for dependency injection

--- justeat_project/src/main/java/com/example/justeat_project/controller/RestaurantController.java
+++ justeat_project/src/main/java/com/example/justeat_project/controller/RestaurantController.java
@@ -1,10 +1,18 @@
 package com.example.justeat_project.controller;
 
 
+import com.example.justeat_project.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestaurantController {
+
+    // autowires the service class for dependency injection
+    @Autowired
+    private RestaurantService restaurantService;
+
 
 
 }
