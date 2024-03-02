af60202616dd13d2d9c0956751e7ea34302fd998
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-02 14:13:35 +0000

    added HTTP GET method to controller

--- justeat_project/src/main/java/com/example/justeat_project/controller/RestaurantController.java
+++ justeat_project/src/main/java/com/example/justeat_project/controller/RestaurantController.java
@@ -4,6 +4,7 @@ package com.example.justeat_project.controller;
 import com.example.justeat_project.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,12 @@ public class RestaurantController {
     @Autowired
     private RestaurantService restaurantService;
 
+    // HTTP GET method to retrieve data for respective postcode input
+    @GetMapping("/restaurants")
 
+    // extracts value of postcode from the HTTP request URL
+    public String getRestaurantByPostcode(@RequestParam String postcode){
+        return restaurantService.getRestaurantData(postcode);
+    }
 
 }
